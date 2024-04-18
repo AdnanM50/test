75c689b107d3326907ea0e62690bccb0e1a3599b
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-04-18 16:49:40 +0600

    almost done

--- my-app/app/(site)/profile/driver/vehicle/page.js
+++ my-app/app/(site)/profile/driver/vehicle/page.js
@@ -75,90 +75,104 @@ const Page = () => {
         <div className='flex justify-between'>
         <PageTitle item="Vehicle Details" />
         <div className=''>
-        <Button 
-        onClick={()=>{push(`/profile/driver/vehicle/edit/${vehicle?._id}`)}}>
-        Update Vehicle
-        </Button>
+        {typeof vehicle === "object" ? 
+         <Button 
+         onClick={()=>{push(`/profile/driver/vehicle/edit/${vehicle?._id}`)}}>
+         Update Vehicle
+         </Button>
+        :<Button 
+          className='cursor-pointer btn btn-success'
+            onClick={()=>{push('/profile/driver/vehicle/add') }}>
+              Add Vehicle
+            </Button>
+        }
+       
         </div>
         </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <DetailTable
-              title="Vehicle Details"
-              columns={[
-                { text: "Vehicle Name", dataIndex: 'name' },
-                { text: "Category", dataIndex: 'category', formatter: (_, d) => d?.category?.name },
-                { text: "model", dataIndex: 'model' },
-                { text: "year", dataIndex: 'year' },
-                {text:"max_power", dataIndex: 'max_power'},
-                {text: "min_power", dataIndex: "min_power"},
-                {text:"capacity",dataIndex:"capacity"},
-                {text:"color",dataIndex:"color"},
-                {text:"fuel_type",dataIndex:"fuel_type"},
-                {text:"mileage",dataIndex:"mileage"},
-                {text:"gear_type",dataIndex:"gear_type"},
-                 // {text:"ac",dataIndex:"ac"},
-                {text:"vehicle_number",dataIndex:"vehicle_number"},
-                // {text:"online",dataIndex:"online"}
-           ]}
-              data={vehicle} 
-              more={(
-              <>
-              <tr className='border-b-2'>
-              <td className="py-2 px-4">AC</td>
-              <td className="py-2 px-4 text-sm ">{vehicle?.ac ? "Yes" : "N/A"}</td>
-              </tr>
-              <tr className='border-b-2'>
-              <td className="py-2 px-4">Online</td>
-              <td className="py-2 px-4 text-sm">{vehicle?.ac ? "Yes" : "N/A"}</td>
-              </tr>
-              </>
-              )}
-              />
-          </div>
-          <div>
-            <div className="border rounded-md p-4 bg-white shadow-md">
-              <h6 className="title">Vehicle Images</h6>
-              <div className="body mt-2">
-                <div className="text-center mb-3" style={{ height: 300 }}>
-                  <img src={vehicle?.images?.[image]} style={{ height: '100%' }} alt="" />
-                </div>
-                <div className="d-flex flex-wrap gap-2">
-                  {vehicle?.images?.map((image, index) => (
-                    <div
-                      key={index}
-                      role="button"
-                      onClick={() => setImage(index)}
-                      style={{ height: 80 }}
-                      className="rounded inline-block text-center bg-white p-2">
-                      <img src={image} style={{ height: '100%' }} alt="" />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="border rounded-md p-4 bg-white shadow-md mt-4">
-              <h6 className="title">Documents</h6>
-              <div className="body mt-2">
-                <div className="text-center mb-3" style={{ height: 300 }}>
-                  <img src={vehicle?.documents?.[document]} style={{ height: '100%' }} alt="" />
-                </div>
-                <div className="d-flex flex-wrap gap-2">
-                  {vehicle?.documents?.map((image, index) => (
-                    <div
-                      key={index}
-                      role="button"
-                      onClick={() => setDocuments(index)}
-                      style={{ height: 80 }}
-                      className="rounded inline-block text-center bg-white p-2">
-                      <img src={image} style={{ height: '100%' }} alt="" />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        {vehicle  ?
+         <div className="grid grid-cols-2 gap-4">
+         <div>
+           <DetailTable
+             title="Vehicle Details"
+             columns={[
+               { text: "Vehicle Name", dataIndex: 'name' },
+               { text: "Category", dataIndex: 'category', formatter: (_, d) => d?.category?.name },
+               { text: "model", dataIndex: 'model' },
+               { text: "year", dataIndex: 'year' },
+               {text:"max_power", dataIndex: 'max_power'},
+               {text: "min_power", dataIndex: "min_power"},
+               {text:"capacity",dataIndex:"capacity"},
+               {text:"color",dataIndex:"color"},
+               {text:"fuel_type",dataIndex:"fuel_type"},
+               {text:"mileage",dataIndex:"mileage"},
+               {text:"gear_type",dataIndex:"gear_type"},
+                // {text:"ac",dataIndex:"ac"},
+               {text:"vehicle_number",dataIndex:"vehicle_number"},
+               // {text:"online",dataIndex:"online"}
+          ]}
+             data={vehicle} 
+             more={(
+             <>
+             <tr className='border-b-2'>
+             <td className="py-2 px-4">AC</td>
+             <td className="py-2 px-4 text-sm ">{vehicle?.ac ? "Yes" : "N/A"}</td>
+             </tr>
+             <tr className='border-b-2'>
+             <td className="py-2 px-4">Online</td>
+             <td className="py-2 px-4 text-sm">{vehicle?.online ? "Yes" : "N/A"}</td>
+             </tr>
+             </>
+             )}
+             />
+         </div>
+         <div>
+           <div className="border rounded-md p-4 bg-white shadow-md">
+             <h6 className="title">Vehicle Images</h6>
+             <div className="body mt-2">
+               <div className="text-center mb-3" style={{ height: 300 }}>
+                 <img src={vehicle?.images?.[image]} style={{ height: '100%' }} alt="" />
+               </div>
+               <div className="d-flex flex-wrap gap-2">
+                 {vehicle?.images?.map((image, index) => (
+                   <div
+                     key={index}
+                     role="button"
+                     onClick={() => setImage(index)}
+                     style={{ height: 80 }}
+                     className="rounded inline-block text-center bg-white p-2">
+                     <img src={image} style={{ height: '100%' }} alt="" />
+                   </div>
+                 ))}
+               </div>
+             </div>
+           </div>
+           <div className="border rounded-md p-4 bg-white shadow-md mt-4">
+             <h6 className="title">Documents</h6>
+             <div className="body mt-2">
+               <div className="text-center mb-3" style={{ height: 300 }}>
+                 <img src={vehicle?.documents?.[document]} style={{ height: '100%' }} alt="" />
+               </div>
+               <div className="d-flex flex-wrap gap-2">
+                 {vehicle?.documents?.map((image, index) => (
+                   <div
+                     key={index}
+                     role="button"
+                     onClick={() => setDocuments(index)}
+                     style={{ height: 80 }}
+                     className="rounded inline-block text-center bg-white p-2">
+                     <img src={image} style={{ height: '100%' }} alt="" />
+                   </div>
+                 ))}
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+        : 
+        <div className='md:text-4xl text-xl flex justify-center items-center text-primary md:my-[150px] my-8'>
+          There is no Vehicle
+        </div>}
+       
       </div>
     );
 };
