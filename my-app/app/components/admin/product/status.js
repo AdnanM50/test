8441535fa8c6d9d97d592c381d7ecf26cf00cc4a
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-04-23 13:42:50 +0600

    some of complete here

--- my-app/app/components/admin/product/status.js
+++ my-app/app/components/admin/product/status.js
@@ -1,23 +1,71 @@
-import { Form, Switch } from "antd";
+import { Switch } from "antd";
+import Form from "antd/es/form/Form";
 import React from "react";
 
 const Status = () => {
   return (
-    <div>
-      <Form
-        onFinish={(value) => {
-          console.log(value);
-        }}
-      >
-        <Form.Item name="avaliable" label="Available / Stock Available">
-          <Switch  /> {/* Corrected */}
-        </Form.Item>
-        <Form.Item name="visibility" label="Stock Visibility">
-          <Switch />
-        </Form.Item>
-        <Form.Item>
+    <div >
+      <Form layout="vertical" className="space-y-8" onFinish={(value)=>{console.log(value)}}>
+        <div className="flex justify-center items-center">
+        <div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Available / Stock available</h1>
+          <Form.Item name="available" >
+          <Switch defaultChecked/>
+          </Form.Item>
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Stock Visibility</h1>
+          <Form.Item name="visibility" >
+          <Switch name="visibility" />
+          </Form.Item>
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Flash Deal</h1>
+          <Form.Item name="flash">
+          <Switch  />
+          </Form.Item>
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>New Arrival</h1>
+          <Form.Item name="Arrival" >
+          <Switch name="Arrival" defaultChecked/>
+          </Form.Item>
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>On Sell</h1>
+          <Switch name="onSell" />
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Best Sell</h1>
+          <Switch name="bestSell" />
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Hot Sell</h1>
+          <Switch name="hotSell" />
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Trending</h1>
+          <Switch name="trending" />
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Featured</h1>
+          <Switch name="featured" />
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Popular</h1>
+          <Switch name="popular" />
+        </div>
+        <div className="flex justify-between mb-4 space-x-12">
+          <h1>Product Active Status</h1>
+          
+          <Switch name="Active" defaultChecked/>
+          </div>
+          </div>
+          </div>
+          <Form.Item>
           <button
-            type="submit"
+            type="submit" // <-- Set type to "submit"
             className="bg-emerald-500 text-white px-[18px] py-2 rounded"
           >
             Submit
--- my-app/app/components/form/multiImage.js
+++ my-app/app/components/form/multiImage.js
@@ -1,13 +1,29 @@
-import { Form, Modal, Upload } from "antd";
+import { Form, Image, Modal, Space, Upload } from "antd";
 import { useState } from "react";
-
-//File Input Component
+import {
+  DownloadOutlined,
+  RotateLeftOutlined,
+  RotateRightOutlined,
+  SwapOutlined,
+  ZoomInOutlined,
+  ZoomOutOutlined,
+} from '@ant-design/icons';
+import Cropper from 'react-easy-crop'
+import {
+  Button,
+  Typography,
+  Slider,
+} from "@mui/material";
+// import ImgDialog from './ImgDialog'
+// import getCroppedImg from './cropImage'
+// import { styles } from './styles'
 const MultipleImageInput = (props) => {
   let max = props.max || 1;
   let name = props.name || "img";
   let label = props.label;
   let listType = props.listType || "picture-card";
 
+ 
   return (
     <div className="form-group">
       <Form.Item
@@ -36,7 +52,7 @@ const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
   const [previewImage, setPreviewImage] = useState("");
   const [previewImage2, setPreviewImage2] = useState("");
 
-  const [images,setImage]=useState(false);
+  const [images, setImage] = useState(false);
   const handleCancel = () => setPreviewVisible(false);
 
   const handlePreview = async (file) => {
@@ -47,19 +63,44 @@ const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
     setPreviewImage(file.url || file.preview);
     setPreviewVisible(true);
   };
-  const [thumb,setThumb]=useState('')
+  const [thumb, setThumb] = useState("");
   const handleChange = async (data) => {
     if (!data?.file?.url && !data?.file?.preview) {
       const preview = await getBase64(data?.file?.originFileObj);
       data = { ...data, file: { ...data.file, preview } }; // Create a new object with the modified preview property
     }
-  
+
     setPreviewImage2(data?.file?.url || data?.file?.preview);
     console.log("handleChange", data);
     setImage(true);
   };
-  
+ // edit image
+ const [crop, setCrop] = useState({ x: 0, y: 0 })
+ const [rotation, setRotation] = useState(0)
+ const [zoom, setZoom] = useState(1)
+ const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+ const [croppedImage, setCroppedImage] = useState(null)
+ const onCropComplete = (croppedArea, croppedAreaPixels) => {
+   setCroppedAreaPixels(croppedAreaPixels)
+ }
 
+ const showCroppedImage = async () => {
+   try {
+     const croppedImage = await getCroppedImg(
+       previewImage2,
+       croppedAreaPixels,
+       rotation
+     )
+     console.log('donee', { croppedImage })
+     setCroppedImage(croppedImage)
+   } catch (e) {
+     console.error(e)
+   }
+ }
+
+ const onClose = () => {
+   setCroppedImage(null)
+ }
   return (
     <>
       <Upload
@@ -71,7 +112,6 @@ const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
         fileList={value || []}
         onChange={handleChange}
         openFileDialogOnClick={true}
-       
         maxCount={max}
       >
         {(value?.length || 0) < max && "+ upload"}
@@ -94,20 +134,82 @@ const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
           <img alt="example" style={{ width: "100%" }} src={previewImage} />
         )}
       </Modal>
-      <Modal   
+      <Modal
         open={images}
         footer={null}
-        onCancel={()=>{setImage(false)}}
-        title={"Edit Image"}>
-  {previewImage2.endsWith(".pdf") ? (
+        onCancel={() => {
+          setImage(false);
+        }}
+        title={"Edit Image"}
+      >
+        {previewImage2.endsWith(".pdf") ? (
           <embed
             src={previewImage2}
             type="application/pdf"
-            width="100%"
-            height="600px"
+           
           />
         ) : (
-          <img alt="edit image" style={{ width: "100%" }} src={previewImage2} />
+          
+          <img
+            alt="edit image"
+            style={{ width: "100%" }}
+            src={previewImage2}
+            className="mt-4"
+          
+          />
+    //   <div className="bg-white">
+    //   <div className="mb-4">
+    //     <Cropper
+    //       image={previewImage2}
+    //       crop={crop}
+    //       rotation={rotation}
+    //       zoom={zoom}
+    //       aspect={4 / 3}
+    //       onCropChange={setCrop}
+    //       onRotationChange={setRotation}
+    //       onCropComplete={onCropComplete}
+    //       onZoomChange={setZoom}
+    //     />
+    //   </div>
+    //   <div style={{position:"static"}}>
+    //     <div >
+    //       <div>
+    //         Zoom
+    //       </div>
+    //       <Slider
+    //         value={zoom}
+    //         min={1}
+    //         max={3}
+    //         step={0.1}
+    //         aria-labelledby="Zoom"
+          
+    //         onChange={(e, zoom) => setZoom(zoom)}
+    //       />
+    //     </div>
+    //     <div >
+    //     <div>
+    //         Rotate
+    //       </div>
+          
+    //       <Slider
+    //         value={rotation}
+    //         min={0}
+    //         max={360}
+    //         step={1}
+    //         aria-labelledby="Rotation"
+    //         onChange={(e, rotation) => setRotation(rotation)}
+    //       />
+    //     </div>
+    //     <Button
+    //       onClick={showCroppedImage}
+    //       variant="contained"
+    //       color="primary"
+    //     >
+    //       Show Result
+    //     </Button>
+    //   </div>
+    //   <Image img={croppedImage} onClose={onClose} />
+    // </div>
         )}
       </Modal>
     </>
@@ -125,8 +227,3 @@ function getBase64(file) {
     reader.onerror = (error) => reject(error);
   });
 }
-
-
-
-
-
