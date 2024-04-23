989ea340e7a84c7d0166447a5567c21d90554cb7
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-04-23 16:15:21 +0600

    multiple image okay

--- my-app/app/components/admin/product/status.js
+++ my-app/app/components/admin/product/status.js
@@ -4,66 +4,85 @@ import React from "react";
 
 const Status = () => {
   return (
-    <div >
-      <Form layout="vertical" className="space-y-8" onFinish={(value)=>{console.log(value)}}>
+    <div>
+      <Form
+        layout="vertical"
+        className="space-y-8"
+        onFinish={(value) => {
+          console.log(value);
+        }}
+      >
         <div className="flex justify-center items-center">
-        <div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Available / Stock available</h1>
-          <Form.Item name="available" >
-          <Switch defaultChecked/>
-          </Form.Item>
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Stock Visibility</h1>
-          <Form.Item name="visibility" >
-          <Switch name="visibility" />
-          </Form.Item>
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Flash Deal</h1>
-          <Form.Item name="flash">
-          <Switch  />
-          </Form.Item>
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>New Arrival</h1>
-          <Form.Item name="Arrival" >
-          <Switch name="Arrival" defaultChecked/>
-          </Form.Item>
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>On Sell</h1>
-          <Switch name="onSell" />
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Best Sell</h1>
-          <Switch name="bestSell" />
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Hot Sell</h1>
-          <Switch name="hotSell" />
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Trending</h1>
-          <Switch name="trending" />
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Featured</h1>
-          <Switch name="featured" />
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Popular</h1>
-          <Switch name="popular" />
-        </div>
-        <div className="flex justify-between mb-4 space-x-12">
-          <h1>Product Active Status</h1>
-          
-          <Switch name="Active" defaultChecked/>
+          <div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Available / Stock available</h1>
+              <Form.Item name="available">
+                <Switch defaultChecked />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Stock Visibility</h1>
+              <Form.Item name="visibility">
+                <Switch name="visibility" />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Flash Deal</h1>
+              <Form.Item name="flash">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>New Arrival</h1>
+              <Form.Item name="Arrival">
+                <Switch name="Arrival" defaultChecked />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>On Sell</h1>
+              <Form.Item name="onSell">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Best Sell</h1>
+              <Form.Item name="bestSell">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Hot Sell</h1>
+              <Form.Item name="hotSell">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Trending</h1>
+              <Form.Item name="trending">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Featured</h1>
+              <Form.Item name="featured">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Popular</h1>
+              <Form.Item name="popular">
+                <Switch />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between mb-4 space-x-12">
+              <h1>Product Active Status</h1>
+              <Form.Item name="Active">
+                <Switch defaultChecked />
+              </Form.Item>
+            </div>
           </div>
-          </div>
-          </div>
-          <Form.Item>
+        </div>
+        <Form.Item>
           <button
             type="submit" // <-- Set type to "submit"
             className="bg-emerald-500 text-white px-[18px] py-2 rounded"
--- my-app/app/components/form/multiImage.js
+++ my-app/app/components/form/multiImage.js
@@ -1,29 +1,14 @@
-import { Form, Image, Modal, Space, Upload } from "antd";
+import { Form, Upload, Modal } from "antd";
 import { useState } from "react";
-import {
-  DownloadOutlined,
-  RotateLeftOutlined,
-  RotateRightOutlined,
-  SwapOutlined,
-  ZoomInOutlined,
-  ZoomOutOutlined,
-} from '@ant-design/icons';
-import Cropper from 'react-easy-crop'
-import {
-  Button,
-  Typography,
-  Slider,
-} from "@mui/material";
-// import ImgDialog from './ImgDialog'
-// import getCroppedImg from './cropImage'
-// import { styles } from './styles'
+import ImgCrop from 'antd-img-crop';
+
+//File Input Component
 const MultipleImageInput = (props) => {
   let max = props.max || 1;
   let name = props.name || "img";
   let label = props.label;
   let listType = props.listType || "picture-card";
 
- 
   return (
     <div className="form-group">
       <Form.Item
@@ -50,9 +35,7 @@ const MultipleImageInput = (props) => {
 const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
   const [previewVisible, setPreviewVisible] = useState(false);
   const [previewImage, setPreviewImage] = useState("");
-  const [previewImage2, setPreviewImage2] = useState("");
 
-  const [images, setImage] = useState(false);
   const handleCancel = () => setPreviewVisible(false);
 
   const handlePreview = async (file) => {
@@ -63,60 +46,27 @@ const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
     setPreviewImage(file.url || file.preview);
     setPreviewVisible(true);
   };
-  const [thumb, setThumb] = useState("");
-  const handleChange = async (data) => {
-    if (!data?.file?.url && !data?.file?.preview) {
-      const preview = await getBase64(data?.file?.originFileObj);
-      data = { ...data, file: { ...data.file, preview } }; // Create a new object with the modified preview property
-    }
 
-    setPreviewImage2(data?.file?.url || data?.file?.preview);
-    console.log("handleChange", data);
-    setImage(true);
+  const handleChange = ({ fileList }) => {
+    onChange(fileList);
   };
- // edit image
- const [crop, setCrop] = useState({ x: 0, y: 0 })
- const [rotation, setRotation] = useState(0)
- const [zoom, setZoom] = useState(1)
- const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
- const [croppedImage, setCroppedImage] = useState(null)
- const onCropComplete = (croppedArea, croppedAreaPixels) => {
-   setCroppedAreaPixels(croppedAreaPixels)
- }
 
- const showCroppedImage = async () => {
-   try {
-     const croppedImage = await getCroppedImg(
-       previewImage2,
-       croppedAreaPixels,
-       rotation
-     )
-     console.log('donee', { croppedImage })
-     setCroppedImage(croppedImage)
-   } catch (e) {
-     console.error(e)
-   }
- }
-
- const onClose = () => {
-   setCroppedImage(null)
- }
   return (
-    <>
-      <Upload
-        accept={`image/png, image/gif, image/jpeg, ${!noWebp && "image/webp"}${
-          pdf ? ", application/pdf" : ""
-        }`}
-        listType={listType}
-        onPreview={handlePreview}
-        fileList={value || []}
-        onChange={handleChange}
-        openFileDialogOnClick={true}
-        maxCount={max}
-      >
-        {(value?.length || 0) < max && "+ upload"}
-      </Upload>
-      {/* <img src={previewImage} alt="image" /> */}
+    <div>
+      <ImgCrop rotationSlider>
+        <Upload
+          accept={`image/png, image/gif, image/jpeg, ${
+            !noWebp && "image/webp"
+          }${pdf ? ", application/pdf" : ""}`}
+          listType={listType}
+          onPreview={handlePreview}
+          fileList={value || []}
+          onChange={handleChange}
+          maxCount={max}
+        >
+          {(value?.length || 0) < max && "+ upload"}
+        </Upload>
+      </ImgCrop> {/* Closing tag for ImgCrop component */}
       <Modal
         open={previewVisible}
         footer={null}
@@ -134,87 +84,10 @@ const Input = ({ value, onChange, listType, max, noWebp, pdf }) => {
           <img alt="example" style={{ width: "100%" }} src={previewImage} />
         )}
       </Modal>
-      <Modal
-        open={images}
-        footer={null}
-        onCancel={() => {
-          setImage(false);
-        }}
-        title={"Edit Image"}
-      >
-        {previewImage2.endsWith(".pdf") ? (
-          <embed
-            src={previewImage2}
-            type="application/pdf"
-           
-          />
-        ) : (
-          
-          <img
-            alt="edit image"
-            style={{ width: "100%" }}
-            src={previewImage2}
-            className="mt-4"
-          
-          />
-    //   <div className="bg-white">
-    //   <div className="mb-4">
-    //     <Cropper
-    //       image={previewImage2}
-    //       crop={crop}
-    //       rotation={rotation}
-    //       zoom={zoom}
-    //       aspect={4 / 3}
-    //       onCropChange={setCrop}
-    //       onRotationChange={setRotation}
-    //       onCropComplete={onCropComplete}
-    //       onZoomChange={setZoom}
-    //     />
-    //   </div>
-    //   <div style={{position:"static"}}>
-    //     <div >
-    //       <div>
-    //         Zoom
-    //       </div>
-    //       <Slider
-    //         value={zoom}
-    //         min={1}
-    //         max={3}
-    //         step={0.1}
-    //         aria-labelledby="Zoom"
-          
-    //         onChange={(e, zoom) => setZoom(zoom)}
-    //       />
-    //     </div>
-    //     <div >
-    //     <div>
-    //         Rotate
-    //       </div>
-          
-    //       <Slider
-    //         value={rotation}
-    //         min={0}
-    //         max={360}
-    //         step={1}
-    //         aria-labelledby="Rotation"
-    //         onChange={(e, rotation) => setRotation(rotation)}
-    //       />
-    //     </div>
-    //     <Button
-    //       onClick={showCroppedImage}
-    //       variant="contained"
-    //       color="primary"
-    //     >
-    //       Show Result
-    //     </Button>
-    //   </div>
-    //   <Image img={croppedImage} onClose={onClose} />
-    // </div>
-        )}
-      </Modal>
-    </>
+    </div>
   );
 };
+
 
 export default MultipleImageInput;
 
@@ -227,3 +100,8 @@ function getBase64(file) {
     reader.onerror = (error) => reject(error);
   });
 }
+
+
+
+
+
