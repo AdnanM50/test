c6a67b49f66a77c596635e645d13f12a7712055f
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-05-01 11:49:57 +0600

    complicated card done

--- my-app/app/components/home/service.js
+++ my-app/app/components/home/service.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Service = () => {
   return (
-    <div className="mb-[150px]">
+    <div className="sm:mb-[120px] mb-[250px]">
       {/* title */}
       <div className="flex justify-center gap-x-[32px] items-center ">
         <div className="w-[26px] h-[0px] border-[5px] border-[#186F65]"></div>
@@ -12,8 +12,8 @@ const Service = () => {
       </div>
       <div className="">
         <div className="container  mt-[40px]">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-[24px] gap-y-[73px] ">
-          <div className="relative  md:h-[274px]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-[24px] md:gap-y-[73px] sm:gap-y-[100px] gap-y-[160px]">
+            <div className=" relative md:h-[274px]">
               <div className="lg:max-w-[424px]  absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
                 <div className="flex flex-col items-center justify-center p-4 relative z-40">
                   <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
@@ -25,99 +25,22 @@ const Service = () => {
                   </p>
                 </div>
               </div>
-              <div className="relative top-[2rem] mx-auto left-[40%] ">
-              <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl">
-              </div>
-              <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
-              <img
-                src="/service1.png"
-                className=" w-[50px] h-[50px]   "
-                alt="image"
-              />
-              </div>
-              </div>
-            </div>
-
-            <div className="relative  md:h-[274px]">
-              <div className="lg:max-w-[424px] absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
-                <div className="flex flex-col items-center justify-center p-4 relative z-40">
-                  <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
-                    Secured Payment Guarantee
-                  </h3>
-                  <p className="text-[#888AA0] font-normal text-center">
-                    By driving with tere you can earn more.By driving with tere
-                    you can earn more.By driving with tere you can earn more.{" "}
-                  </p>
+              <div className="relative top-[2rem]  h-[200px] flex justify-center ">
+                <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl"></div>
+                <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
+                  <img
+                    src="/service1.png"
+                    className=" w-[50px] h-[50px]   "
+                    alt="image"
+                  />
                 </div>
               </div>
-              <div className="relative top-[2rem] mx-auto left-[40%] ">
-              <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl">
-              </div>
-              <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
-              <img
-                src="/service1.png"
-                className=" w-[50px] h-[50px]   "
-                alt="image"
-              />
-              </div>
-              </div>
             </div>
-
-
-            <div className="relative  md:h-[274px]">
-              <div className="lg:max-w-[424px] absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
-                <div className="flex flex-col items-center justify-center p-4 relative z-40">
-                  <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
-                    Secured Payment Guarantee
-                  </h3>
-                  <p className="text-[#888AA0] font-normal text-center">
-                    By driving with tere you can earn more.By driving with tere
-                    you can earn more.By driving with tere you can earn more.{" "}
-                  </p>
-                </div>
-              </div>
-              <div className="relative top-[2rem] mx-auto left-[40%] ">
-              <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl">
-              </div>
-              <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
-              <img
-                src="/service1.png"
-                className=" w-[50px] h-[50px]   "
-                alt="image"
-              />
-              </div>
-              </div>
-            </div>
-            <div className="relative  md:h-[274px]">
-              <div className="lg:max-w-[424px] absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
-                <div className="flex flex-col items-center justify-center p-4 relative z-40">
-                  <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
-                    Secured Payment Guarantee
-                  </h3>
-                  <p className="text-[#888AA0] font-normal text-center">
-                    By driving with tere you can earn more.By driving with tere
-                    you can earn more.By driving with tere you can earn more.{" "}
-                  </p>
-                </div>
-              </div>
-              <div className="relative top-[2rem] mx-auto left-[40%] ">
-              <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl">
-              </div>
-              <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
-              <img
-                src="/service1.png"
-                className=" w-[50px] h-[50px]   "
-                alt="image"
-              />
-              </div>
-              </div>
-            </div>
-
             <div className="relative   md:h-[274px]">
-              <div className="lg:max-w-[424px] absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
+              <div className="lg:max-w-[424px]  absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
                 <div className="flex flex-col items-center justify-center p-4 relative z-40">
                   <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
-                    Secured Payment Guarantee
+                    Help Center & Support 24/7
                   </h3>
                   <p className="text-[#888AA0] font-normal text-center">
                     By driving with tere you can earn more.By driving with tere
@@ -125,25 +48,22 @@ const Service = () => {
                   </p>
                 </div>
               </div>
-              <div className="relative top-[2rem] mx-auto left-[40%] ">
-              <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl">
-              </div>
-              <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
-              <img
-                src="/service1.png"
-                className=" w-[50px] h-[50px]   "
-                alt="image"
-              />
-              </div>
+              <div className="relative top-[2rem]  h-[200px] flex justify-center ">
+                <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl"></div>
+                <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
+                  <img
+                    src="/service2.png"
+                    className=" w-[50px] h-[50px]   "
+                    alt="image"
+                  />
+                </div>
               </div>
             </div>
-
-
-            <div className="relative  md:h-[274px]">
-              <div className="lg:max-w-[424px] absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
+            <div className="relative   md:h-[274px]">
+              <div className="lg:max-w-[424px]  absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
                 <div className="flex flex-col items-center justify-center p-4 relative z-40">
                   <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
-                    Secured Payment Guarantee
+                    Booking any Class Vehicles
                   </h3>
                   <p className="text-[#888AA0] font-normal text-center">
                     By driving with tere you can earn more.By driving with tere
@@ -151,27 +71,86 @@ const Service = () => {
                   </p>
                 </div>
               </div>
-              <div className="relative top-[2rem] mx-auto left-[40%] ">
-              <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl">
-              </div>
-              <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
-              <img
-                src="/service1.png"
-                className=" w-[50px] h-[50px]   "
-                alt="image"
-              />
-              </div>
+              <div className="relative top-[2rem]  h-[200px] flex justify-center ">
+                <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl"></div>
+                <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
+                  <img
+                    src="/service3.png"
+                    className=" w-[50px] h-[50px]   "
+                    alt="image"
+                  />
+                </div>
               </div>
             </div>
-            {/* <div className="relative h-[400px] w-[400px] bg-pink-600 z-50">
-              <Image
-                src="/service1.png"
-                width={100}
-                height={100}
-                className="h-[100px] w-[100px] object-fill absolute top-[-5rem] z-20"
-                alt="image"
-              />
-            </div> */}
+            <div className=" relative md:h-[274px]">
+              <div className="lg:max-w-[424px]  absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
+                <div className="flex flex-col items-center justify-center p-4 relative z-40">
+                  <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
+                    Corporate and Business Services
+                  </h3>
+                  <p className="text-[#888AA0] font-normal text-center">
+                    By driving with tere you can earn more.By driving with tere
+                    you can earn more.By driving with tere you can earn more.{" "}
+                  </p>
+                </div>
+              </div>
+              <div className="relative top-[2rem]  h-[200px] flex justify-center ">
+                <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl"></div>
+                <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
+                  <img
+                    src="/service4.png"
+                    className=" w-[50px] h-[50px]   "
+                    alt="image"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="relative   md:h-[274px]">
+              <div className="lg:max-w-[424px]  absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
+                <div className="flex flex-col items-center justify-center p-4 relative z-40">
+                  <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
+                  Car Sharing Options                  
+                  </h3>
+                  <p className="text-[#888AA0] font-normal text-center">
+                    By driving with tere you can earn more.By driving with tere
+                    you can earn more.By driving with tere you can earn more.{" "}
+                  </p>
+                </div>
+              </div>
+              <div className="relative top-[2rem]  h-[200px] flex justify-center ">
+                <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl"></div>
+                <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
+                  <img
+                    src="/service5.png"
+                    className=" w-[50px] h-[50px]   "
+                    alt="image"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="relative   md:h-[274px]">
+              <div className="lg:max-w-[424px]  absolute w-auto h-auto md:h-[274px] custom-rounded2 bg-white px-[20px] pb-[40px] shadow_custom2 mt-[83px] !z-50  ">
+                <div className="flex flex-col items-center justify-center p-4 relative z-40">
+                  <h3 className="text-[#0B204C] font-normal text-center lg:max-w-[312px] text-[24px] mt-[28px] mb-[26px] capitalize">
+                  Limousine and Chauffeur Hire                  
+                  </h3>
+                  <p className="text-[#888AA0] font-normal text-center">
+                    By driving with tere you can earn more.By driving with tere
+                    you can earn more.By driving with tere you can earn more.{" "}
+                  </p>
+                </div>
+              </div>
+              <div className="relative top-[2rem]  h-[200px] flex justify-center ">
+                <div className=" absolute  bg-red-300  z-10 rounded-full shadow_custom2 h-[97.59px] w-[100px] shadow-xl"></div>
+                <div className="bg-white h-[97.59px] w-[100px] rounded-full absolute  z-50   flex justify-center items-center">
+                  <img
+                    src="/service6.png"
+                    className=" w-[50px] h-[50px]   "
+                    alt="image"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
